46f5ffa02fa5378a33900c66d77efb39fcaed535
James Gaboardi <jgaboardi@gmail.com>
2018-08-27 23:03:08 -0400

    adding tearDown in tests

--- spaghetti/tests/test_network_api_from_gdf.py
+++ spaghetti/tests/test_network_api_from_gdf.py
@@ -11,15 +11,15 @@ except ImportError:
 
 @unittest.skipIf(GEOPANDAS_EXTINCT, 'Missing Geopandas')
 class TestNetwork(unittest.TestCase):
-
+    
     def setUp(self):
         path_to_shp = examples.get_path('streets.shp')
         gdf = geopandas.read_file(path_to_shp)
         self.ntw = spgh.Network(in_data=gdf)
-
+    
     def tearDown(self):
         pass
-
+    
     def test_extract_network(self):
         self.assertEqual(len(self.ntw.edges), 303)
         self.assertEqual(len(self.ntw.nodes), 230)
@@ -28,19 +28,19 @@ class TestNetwork(unittest.TestCase):
         self.assertIn(0, self.ntw.adjacencylist[1])
         self.assertIn(0, self.ntw.adjacencylist[2])
         self.assertNotIn(0, self.ntw.adjacencylist[3])
-
+    
     def test_contiguity_weights(self):
         w = self.ntw.contiguityweights(graph=False)
         self.assertEqual(w.n, 303)
         self.assertEqual(w.histogram,
                          [(2, 35), (3, 89), (4, 105), (5, 61), (6, 13)])
-
+    
     def test_contiguity_weights_graph(self):
         w = self.ntw.contiguityweights(graph=True)
         self.assertEqual(w.n, 179)
         self.assertEqual(w.histogram,
                          [(2, 2), (3, 2), (4, 45), (5, 82), (6, 48)])
-
+    
     def test_distance_band_weights(self):
         # I do not trust this result, test should be manually checked.
         w = self.ntw.distancebandweights(threshold=500)
@@ -48,12 +48,12 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(w.histogram,
                          [(1, 22), (2, 58), (3, 63), (4, 40),
                           (5, 36), (6, 3), (7, 5), (8, 3)])
-
+    
     def test_edge_segmentation(self):
         n200 = self.ntw.segment_edges(200.0)
         self.assertEqual(len(n200.edges), 688)
         n200 = None
-
+    
     def test_enum_links_node(self):
         coincident = self.ntw.enum_links_node(24)
         self.assertIn((24, 48), coincident)
@@ -61,7 +61,7 @@ class TestNetwork(unittest.TestCase):
 
 @unittest.skipIf(GEOPANDAS_EXTINCT, 'Missing Geopandas')
 class TestNetworkPointPattern(unittest.TestCase):
-
+    
     def setUp(self):
         path_to_shp = examples.get_path('streets.shp')
         gdf = geopandas.read_file(path_to_shp)
@@ -70,35 +70,35 @@ class TestNetworkPointPattern(unittest.TestCase):
             in_data = examples.get_path('{}.shp'.format(obs))
             self.ntw.snapobservations(in_data, obs, attribute=True)
             setattr(self, obs, self.ntw.pointpatterns[obs])
-
+    
     def tearDown(self):
         pass
-
+    
     def test_add_point_pattern(self):
         self.assertEqual(self.crimes.npoints, 287)
         self.assertIn('properties', self.crimes.points[0])
         self.assertIn([1, 1], self.crimes.points[0]['properties'])
-
+    
     def test_count_per_edge(self):
         counts = self.ntw.count_per_edge(
             self.ntw.pointpatterns['crimes'].obs_to_edge, graph=False)
         meancounts = sum(counts.values()) / float(len(counts.keys()))
         self.assertAlmostEqual(meancounts, 2.682242, places=5)
-
+    
     def test_count_per_graph_edge(self):
         counts = self.ntw.count_per_edge(
             self.ntw.pointpatterns['crimes'].obs_to_edge, graph=True)
         meancounts = sum(counts.values()) / float(len(counts.keys()))
         self.assertAlmostEqual(meancounts, 3.29885, places=5)
-
+    
     def test_simulate_normal_observations(self):
         npoints = self.ntw.pointpatterns['crimes'].npoints
         sim = self.ntw.simulate_observations(npoints)
         self.assertEqual(npoints, sim.npoints)
-
+    
     def test_simulate_poisson_observations(self):
         pass
-
+    
     def test_all_neighbor_distances(self):
         distancematrix_1 = self.ntw.allneighbordistances(self.schools)
         self.assertAlmostEqual(np.nansum(distancematrix_1[0]),
@@ -115,7 +115,7 @@ class TestNetworkPointPattern(unittest.TestCase):
         observed = distancematrix_2.diagonal()
         known = np.zeros(distancematrix_2.shape[0])
         np.testing.assert_equal(observed, known)
-
+    
     def test_nearest_neighbor_distances(self):
         with self.assertRaises(KeyError):
             self.ntw.nearestneighbordistances('i_should_not_exist')
@@ -123,18 +123,21 @@ class TestNetworkPointPattern(unittest.TestCase):
         nnd2 = self.ntw.nearestneighbordistances('schools',
                                                  'schools')
         np.testing.assert_array_equal(nnd, nnd2)
-
+    
     def test_nearest_neighbor_search(self):
         pass
 
 
 @unittest.skipIf(GEOPANDAS_EXTINCT, 'Missing Geopandas')
 class TestNetworkUtils(unittest.TestCase):
-
+    
     def setUp(self):
         path_to_shp = examples.get_path('streets.shp')
         gdf = geopandas.read_file(path_to_shp)
         self.ntw = spgh.Network(in_data=gdf)
+    
+    def tearDown(self):
+        pass
     
     def test_compute_length(self):
         self.point1, self.point2 = (0,0), (1,1)
