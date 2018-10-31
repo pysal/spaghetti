45f17c6d0d57a30acee2796b0bdafa78b0df71d3
James Gaboardi <jgaboardi@gmail.com>
2018-10-31 18:48:54 -0400

    adding one space

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """Spatial Network Analysis (SPAtial GrapHs: nETworks, Topology, & Inference) This package is part of a refactoring of PySAL. Spaghetti is an open-source python library for the analysis of network-based spatial data. Originating from the ``network`` module in PySAL (Python Spatial Analysis Library), it is under active development for the inclusion of newly proposed methods for building graph-theoretic networks and the analysis of network events.
 """
 
+
 DOCLINES = __doc__.split("\n")
 
 with open('README.rst', 'r', encoding='utf8') as file:
