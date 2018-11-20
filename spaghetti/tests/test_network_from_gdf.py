4e61ccf7addf2d47b5195c6f070c855b9f6184f5
James Gaboardi <jgaboardi@gmail.com>
2018-11-19 21:00:12 -0500

    add multiprocesing test to test_network_from_gdf.py

--- spaghetti/tests/test_network_from_gdf.py
+++ spaghetti/tests/test_network_from_gdf.py
@@ -140,6 +140,13 @@ class TestNetworkPointPattern(unittest.TestCase):
         known_mtx_val = 3218.2597894
         observed_mtx_val = matrix3
         self.assertAlmostEqual(observed_mtx_val[0, 1], known_mtx_val, places=4)
+        
+        matrix4 = self.ntw.allneighbordistances('schools',
+                                                fill_diagonal=0.,
+                                                n_processes=2)
+        observed = matrix4.diagonal()
+        known = np.zeros(matrix4.shape[0])
+        self.assertEqual(observed.all(), known.all())
     
     def test_nearest_neighbor_distances(self):
         # general test
