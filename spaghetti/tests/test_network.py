ede8588997b1a5f8a32bbe43d524c1ab75807547
James Gaboardi <jgaboardi@gmail.com>
2018-11-23 19:24:13 -0500

    removing extra indent[2]

--- spaghetti/tests/test_network.py
+++ spaghetti/tests/test_network.py
@@ -108,8 +108,8 @@ class TestNetwork(unittest.TestCase):
         self.ntw_from_shp.node_sig = 2
         obs_xy_round2 = self.ntw_from_shp._round_sig((1215, 1865))
         self.assertEqual(obs_xy_round2, (x_round2, y_round2))
-            
-            
+        
+        
         # round to no significant digits test
         x_roundNone, y_roundNone = 1215, 1865
         self.ntw_from_shp.node_sig = None
