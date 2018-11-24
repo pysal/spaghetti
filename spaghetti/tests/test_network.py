80d53a186ab18875f6bef3fdaf2063b857946c71
James Gaboardi <jgaboardi@gmail.com>
2018-11-23 19:22:12 -0500

    add line space between class

--- spaghetti/tests/test_network.py
+++ spaghetti/tests/test_network.py
@@ -109,6 +109,7 @@ class TestNetwork(unittest.TestCase):
             obs_xy_round2 = self.ntw_from_shp._round_sig((1215, 1865))
             self.assertEqual(obs_xy_round2, (x_round2, y_round2))
 
+
 @unittest.skipIf(GEOPANDAS_EXTINCT, 'Missing Geopandas')
 class TestNetworkPointPattern(unittest.TestCase):
     
