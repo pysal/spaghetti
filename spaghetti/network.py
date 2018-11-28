59c03bc1469edc767bb738d50783af017b0310b3
James Gaboardi <jgaboardi@gmail.com>
2018-11-28 17:31:22 -0500

    pep8 doc review [22]

--- spaghetti/network.py
+++ spaghetti/network.py
@@ -1368,26 +1368,27 @@ class Network:
             A spaghetti point pattern object.
         
         nsteps : int
-            The number of steps at which the count of the nearest neighbors
-            is computed.
+            The number of steps at which the count of the nearest
+            neighbors is computed.
         
         permutations : int
-            The number of permutations to perform (default 99).
+            The number of permutations to perform. Default 99.
         
         threshold : float
             The level at which significance is computed.
-            -- 0.5 would be 97.5% and 2.5%
+            0.5 would be 97.5% and 2.5%
         
         distribution : str
-            The distribution from which random points are sampled
-            -- uniform or poisson
+            The distribution from which random points are sampled.
+            Either ``"uniform"`` or ``"poisson"``.
         
         lowerbound : float
-            The lower bound at which the F-function is computed. (Default 0).
+            The lower bound at which the F-function is computed.
+            Default 0.
         
         upperbound : float
-            The upper bound at which the F-function is computed. Defaults to
-            the maximum observed nearest neighbor distance.
+            The upper bound at which the F-function is computed.
+            Defaults to the maximum observed nearest neighbor distance.
         
         Returns
         -------
