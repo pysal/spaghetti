1a78fae9ff32b3675f780dde740372efa0bb8a1e
James Gaboardi <jgaboardi@gmail.com>
2019-12-31 22:05:46 -0500

    trying new binder link

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -268,7 +268,7 @@ intersphinx_mapping = {
 
 # This is processed by Jinja2 and inserted before each notebook
 nbsphinx_prolog = r"""
-{% set docname = env.doc2path(env.docname, base='notebooks') %}
+{% set docname = env.doc2path(env.docname, base='') %}
 
 .. only:: html
 
