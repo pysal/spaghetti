3f8d2dbe485b91e7c38250db92938133d718d053
James Gaboardi <jgaboardi@gmail.com>
2020-05-03 20:32:47 -0400

    bump version to upstream

--- spaghetti/__init__.py
+++ spaghetti/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.5.0.rc12"
+__version__ = "1.5.0.rc13"
 """
 :mod:`spaghetti` --- Spatial Graphs: Networks, Topology, & Inference
 ====================================================================
