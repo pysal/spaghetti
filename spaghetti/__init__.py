7929dd3772ba7893c95b3a5a83ccbc5899564c06
James Gaboardi <jgaboardi@gmail.com>
2020-10-03 12:06:33 -0400

    update __init__.py

--- spaghetti/__init__.py
+++ spaghetti/__init__.py
@@ -1,9 +1,9 @@
 __version__ = "1.5.0"
-"""
-:mod:`spaghetti` --- Spatial Graphs: Networks, Topology, & Inference
-====================================================================
 
 """
+# `spaghetti` --- Spatial Graphs: Networks, Topology, & Inference
+"""
+
 from .network import Network, PointPattern, SimulatedPointPattern
 from .network import extract_component, spanning_tree
 from .network import element_as_gdf, regular_lattice
